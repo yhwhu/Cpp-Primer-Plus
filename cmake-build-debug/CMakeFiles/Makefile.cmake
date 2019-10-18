50d0599000e374e7cbe0f36fcf805772686e12f0
yhwhu <13207106053@163.com>
2019-10-18 13:30:07 +0800

    1. 使用自带的编译器 apple LLVM 2. 运行多个cpp文件 3. 给main函数添加参数运行

--- cmake-build-debug/CMakeFiles/Makefile.cmake
+++ cmake-build-debug/CMakeFiles/Makefile.cmake
@@ -125,5 +125,5 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/Cpp-Primer-Plus.dir/DependInfo.cmake"
+  "CMakeFiles/Cpp_Primer_Plus.dir/DependInfo.cmake"
   )
